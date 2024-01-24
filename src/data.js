3292c153e56c76a60e010212966c15caa75d3e97
Adekunle Adesokan <kunle.adesokan9@gmail.com>
2024-01-24 04:55:18 +0100

    update faq data

--- src/data.js
+++ src/data.js
@@ -9,26 +9,26 @@ const faqs = [
     id: 1,
     question: "Can I use ClearLink on multiple devices?",
     answer:
-      "ClearLink offers flexible meeting options. Depending on your subscription plan, you can host meetings with varying numbers of participants. Our plans are designed to accommodate small team collaborations and large-scale webinars, ensuring you have the right fit for your needs.",
+      "Yes, ClearLink is designed to be accessible across various devices. You can seamlessly use it on your desktop, laptop, tablet, or smartphone.",
   },
   {
     id: 2,
     question:
       "Is ClearLink compatible with other video conferencing platforms?",
     answer:
-      "ClearLink offers flexible meeting options. Depending on your subscription plan, you can host meetings with varying numbers of participants. Our plans are designed to accommodate small team collaborations and large-scale webinars, ensuring you have theright fit for your needs.",
+      "Absolutely! ClearLink is built with interoperability in mind. It works seamlessly with popular video conferencing platforms like Zoom, Microsoft Teams, and others, ensuring a smooth collaboration experience.",
   },
   {
     id: 3,
     question: "How does ClearLink ensure the security of my video conferences?",
     answer:
-      "ClearLink offers flexible meeting options. Depending on your subscription plan, you can host meetings with varying numbers of participants. Our plans are designed to accommodate small team collaborations and large-scale webinars, ensuring you have theright fit for your needs.",
+      "ClearLink prioritizes the security of your video conferences. It employs end-to-end encryption, secure access controls, and follows industry best practices to safeguard your meetings from unauthorized access and protect your sensitive information.",
   },
   {
     id: 4,
     question: "Do I need to download any software to use ClearLink?",
     answer:
-      "ClearLink offers flexible meeting options. Depending on your subscription plan, you can host meetings with varying numbers of participants. Our plans are designed to accommodate small team collaborations and large-scale webinars, ensuring you have theright fit for your needs.",
+      "No need for additional software downloads! ClearLink is accessible directly through your web browser, offering a hassle-free experience without the need for installations. Just click and start your video conference instantly.",
   },
 ];
 
